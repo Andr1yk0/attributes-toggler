a138a3533fee19a6861cc8655ee89ce8b53b325e
Andriy Lozynskiy <alozynskiy@gmail.com>
2017-09-18 22:59:07 +0300

    basic functional

---
+++ spec/javascripts/mainSpec.js
@@ -0,0 +1,77 @@
+describe('AttributeToggler', function () {
+    var defaultTrigger = 'click';
+    var eventNamespace = '.toggler';
+
+
+   it('tests are ready', function () {
+       expect(AttributeToggler).toBeDefined();
+       expect(loadFixtures).toBeDefined();
+       loadFixtures('toggleClassOnDefault.html');
+       expect($('#toggleClassOnDefault')).toExist();
+   });
+
+   it('can find elements by attribute and prepare them', function () {
+        loadFixtures('toggleClassOnDefault.html');
+        AttributeToggler.init();
+        var expectVal = {
+            domEl:$('#toggleClassOnDefault'),
+            trigger: defaultTrigger,
+            attrName: 'class',
+            value: 'two'
+        };
+        expect(AttributeToggler.getElements()[0]).toEqual(expectVal)
+   });
+
+
+   
+   it('can toggle attributes by click', function () {
+       loadFixtures('toggleClassOnDefault.html');
+       AttributeToggler.init();
+       var $toggleClassOnDefault = $('#toggleClassOnDefault');
+       expect($toggleClassOnDefault).toHaveClass('one');
+
+       $toggleClassOnDefault.trigger('click');
+       expect($toggleClassOnDefault).toHaveClass('two');
+
+       $toggleClassOnDefault.trigger('click');
+       expect($toggleClassOnDefault).toHaveClass('one');
+   });
+
+   it('can toggle attributes by hover', function () {
+       loadFixtures('toggleHrefOnHover.html');
+       AttributeToggler.init();
+       var $toggleHrefOnHover = $('#toggleHrefOnHover');
+       expect(AttributeToggler.getElements()[0].trigger).toEqual('mouseenter'+eventNamespace+' mouseleave'+eventNamespace);
+
+
+       expect($toggleHrefOnHover.attr('href')).toEqual('one');
+       $toggleHrefOnHover.trigger('mouseenter');
+       expect($toggleHrefOnHover.attr('href')).toEqual('two');
+
+       $toggleHrefOnHover.trigger('mouseleave');
+       expect($toggleHrefOnHover.attr('href')).toEqual('one');
+   });
+
+    it('can stop toggling for all elements', function () {
+        loadFixtures('toggleClassOnDefault.html','toggleHrefOnHover.html');
+        AttributeToggler.init();
+        var $toggleClassOnDefault = $('#toggleClassOnDefault'), $toggleHrefOnHover = $('#toggleHrefOnHover');
+        $toggleClassOnDefault.trigger('click');
+        $toggleHrefOnHover.trigger('mouseenter');
+        expect($toggleClassOnDefault).toHaveClass('two');
+        expect($toggleHrefOnHover.attr('href')).toEqual('two');
+
+        $toggleClassOnDefault.trigger('click');
+        $toggleHrefOnHover.trigger('mouseleave');
+        expect($toggleClassOnDefault).toHaveClass('one');
+        expect($toggleHrefOnHover.attr('href')).toEqual('one');
+
+        AttributeToggler.stop();
+        $toggleClassOnDefault.trigger('click');
+        $toggleHrefOnHover.trigger('mouseenter');
+        expect($toggleClassOnDefault).toHaveClass('one');
+        expect($toggleHrefOnHover.attr('href')).toEqual('one');
+
+    });
+
+});
---
+++ src/index.js
@@ -0,0 +1,60 @@
+var AttributeToggler = (function () {
+    var elements = [];
+    var trigger = 'click';
+    var eventNamespace = '.toggler';
+    var triggerAliaces = {
+        hover:'mouseenter'+eventNamespace+' mouseleave'+eventNamespace
+    };
+    var init = function (options) {
+        if(elements.length>0){
+            stop();
+        }
+        var all = $('[data-toggle]');
+        all.each(function (i,e) {
+            var event = $(e).data('toggle-trigger');
+            if(triggerAliaces.hasOwnProperty(event)){
+                event = triggerAliaces[event];
+            }
+            var newEl = {
+                domEl:$(e),
+                attrName:$(e).data('toggle'),
+                trigger:event?event:trigger,
+                value:$(e).data('toggle-value')
+            };
+            elements.push(newEl);
+        });
+        
+        $.each(elements,function (i,obj) {
+            _toggle(obj)
+        })
+    };
+
+    var stop = function () {
+        $.each(elements, function (i,el) {
+            $(el.domEl).off(eventNamespace);
+        });
+        elements = [];
+    };
+
+    var getElements = function () {
+      return elements
+    };
+    
+    var _toggle = function (obj) {
+        $(obj.domEl).on(obj.trigger+eventNamespace, function () {
+            var el = $(this);
+            var newVal = el.attr('data-toggle-value');
+            if(!newVal){
+                el.attr('data-toggle-value',obj.value)
+                newVal = obj.value;
+            }
+            el.attr('data-toggle-value',el.attr(obj.attrName)).attr(obj.attrName,newVal);
+        })
+    };
+
+    return {
+        getElements:getElements,
+        init:init,
+        stop:stop
+    }
+})();
